909f08c38eea7989e6c2d8ef8156c67a000cadc6
Josh Everhart <jeverhart.17316@icloud.com>
2025-04-13 17:48:04 -0400

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// Get the GitHub repository name
+const repoName = process.env.GITHUB_REPOSITORY
+  ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`
+  : "";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? repoName : "/",
   plugins: [vue()],
-})
+});
