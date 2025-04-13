0b9a4b1b4b4e23b52ff9620e75f8a49b8a9b1be6
Josh Everhart <jeverhart.17316@icloud.com>
2025-04-13 17:57:01 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/my-vue-app/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/my-vue-app/dist/" : "/",
   plugins: [vue()],
 });
